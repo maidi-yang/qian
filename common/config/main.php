67a5e1520c51c33950c84b4f6764b6d3efe15a29
maidi.yang <maidi.yang@shareworks.cn>
2018-04-27 11:05:29 +0800

    db

--- common/config/main.php
+++ common/config/main.php
@@ -1,5 +1,5 @@
 <?php
-return [
+$config =  [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
@@ -15,18 +15,30 @@ return [
 //                 ],
 //             ],
 //         ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yiiboot3',
-            'username' => 'root',
-            'password' => 'root',
-            'charset' => 'utf8',
-        ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
             'useFileTransport' => true,
         ],
     ],
-    
 ];
+if(ERRAND_ENV_TEST){
+    $config['components']['db'] = [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yiiboot3',
+        'username' => 'root',
+        'password' => 'root',
+        'charset' => 'utf8',
+    ];
+}else{
+    $config['components']['db'] = [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yiiboot3',
+        'username' => 'root',
+        'password' => 'qian!2018',
+        'charset' => 'utf8',
+    ];
+}
+return $config;
+
+
---
+++ env.php
@@ -0,0 +1,2 @@
+<?php
+defined('ERRAND_ENV_TEST') or define('ERRAND_ENV_TEST', true);
